eaa2a0fdafcf47c7539b715b7fbc2ddfb1ffe947
Vinicius Morais <vinidev.morais@gmail.com>
2024-01-24 10:12:53 -0300

    created class Account

--- Entities/Account.cs
+++ Entities/Account.cs
@@ -1,12 +1,40 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using ExercicioExcecao.Entities.AccountExceptions;
 
 namespace ExercicioExcecao.Entities
 {
     public class Account
     {
+        public int Number { get; set; }
+        public string Holder { get; set; }
+        public double Balance { get; set; }
+        public double WithdrawLimit { get; set; }
         
+        public Account ()
+        {
+        }
+        public Account (int number, string holder, double balance, double withdrawLimit)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+            WithdrawLimit = withdrawLimit;
+        }
+        
+        public void Deposit(double amount)
+        {
+            Balance += amount;
+        }
+        public void Withdraw(double amount)
+        {
+            if (amount > WithdrawLimit)
+            {
+                throw new DomainException ("This amount is higher than the withdrawal limit");
+            }
+            if (amount > Balance)
+            {
+                throw new DomainException("It is impossible to withdraw as the amount is greater than the value in the bank");
+            }
+        }
+                
     }
 }
